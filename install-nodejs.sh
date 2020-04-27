6c34606a73c6e5e4e59064abe3a430c4dced623b
Valdir Junior <junior_vs@outlook.com>
2020-04-26 22:36:29 -0300

    ebluoli

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -2,6 +2,6 @@
 
 # https://github.com/nodesource/distributions/blob/master/README.md#debinstall
 
-curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 sudo npm i -g npm
---
+++ oracle-jdk-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo add-apt-repository ppa:linuxuprising/java
+sudo apt update
+sudo apt -y install oracle-java14-installer
